41c9f0acf32a86deab484f259910d06b88b369a1
marcoplaxma <mm@upsell.ai>
2019-06-02 19:18:04 -0700

    consistent score separation

--- components/Results.js
+++ components/Results.js
@@ -7,7 +7,7 @@ class Results extends Component {
                 <div className='marcador'>
                     <div className='marcador_player'>ME</div>
                     <div className='marcador_value'>{this.props.score.user}</div>
-                    <div>
+                    <div className='result_gif_container'>
                     	{
                     		this.props.gifs.user ? 
 	                    		<img className='result_gif' src={this.props.gifs.user} alt='user_gif' /> :
@@ -18,7 +18,7 @@ class Results extends Component {
                 <div className='marcador'>
                     <div className='marcador_player'>CPU</div>
                     <div className='marcador_value'>{this.props.score.cpu}</div>
-                    <div>
+                    <div className='result_gif_container'>
                     	{
                     		this.props.gifs.cpu ?
                     			<img className='result_gif' src={this.props.gifs.cpu} alt='cpu_gif' /> :
